3e9779b5bf943d70b0a522153a91b2c1207a255b
Edward Humpert <edwardmhumpert@gmail.com>
2023-03-24 19:59:48 -0400

    rando keyboard working

--- index.js
+++ index.js
@@ -11,13 +11,13 @@
 
 // let's get shuffle keyboard function down
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const keys = document.querySelectorAll(".key");
   const textField = document.getElementById("text-field");
   const currentValue = textField.value;
   const clearBtn = document.querySelector(".clear");
   const deleteBtn = document.querySelector(".delete");
+  const keyboardBtn = document.querySelector(".newKeyboard");
   let timer = document.getElementById("timer");
   let startStopButton = document.getElementById("start-stop");
   let intervalId;
@@ -34,6 +34,113 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   deleteBtn.addEventListener("click", deleteLastCharacter);
+
+  keyboardBtn.addEventListener("click", newKeyboard);
+
+  function newKeyboard() {
+    // get rows, clear rows, 
+    const buttonsToClear = document.querySelectorAll("button.key");
+    deleteBtn.remove();
+    clearBtn.remove();
+    buttonsToClear.forEach((button) => button.remove());
+    // populate rows.
+    // data store of ABCs needed
+    const alphabet = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    // shuffle array
+    const useArray = shuffleArray(alphabet);
+    // populate buttons 0-5, delete, 6-12, 13-19, 20-25, clear
+    // for loops?
+    //ROW 1
+    const row1 = document.querySelector(".row1");
+    for (let i = 0; i < 6; i++) {
+      const button = document.createElement("button");
+      // Set the class and data-key attributes
+      button.classList.add("key");
+      button.textContent = useArray[i].toUpperCase();
+      row1.appendChild(button);
+    }
+
+    //add delete button
+    const deleteButton = document.createElement("button");
+      // Set the class and data-key attributes
+      deleteButton.classList.add("key");
+      deleteButton.classList.add("delete");
+      deleteButton.textContent = "Delete";
+      row1.appendChild(deleteButton);
+
+    //ROW 2
+    const row2 = document.querySelector(".row2");
+    for (let i = 6; i < 13; i++) {
+      const button = document.createElement("button");
+      // Set the class and data-key attributes
+      button.classList.add("key");
+      button.textContent = useArray[i].toUpperCase();
+      row2.appendChild(button);
+    }
+
+    //ROW 3
+    const row3 = document.querySelector(".row3");
+    for (let i = 13; i < 20; i++) {
+      const button = document.createElement("button");
+      // Set the class and data-key attributes
+      button.classList.add("key");
+      button.textContent = useArray[i].toUpperCase();
+      row3.appendChild(button);
+    }
+
+    //ROW 4
+    const row4 = document.querySelector(".row4");
+    for (let i = 20; i < 26; i++) {
+      const button = document.createElement("button");
+      // Set the class and data-key attributes
+      button.classList.add("key");
+      button.textContent = useArray[i].toUpperCase();
+      row4.appendChild(button);
+    }
+
+    //add clear button
+    const clearButton = document.createElement("button");
+      // Set the class and data-key attributes
+      clearButton.classList.add("key");
+      clearButton.classList.add("clear");
+      clearButton.textContent = "Clear";
+      row4.appendChild(clearButton);
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   function deleteLastCharacter() {
     const textField = document.getElementById("text-field");
