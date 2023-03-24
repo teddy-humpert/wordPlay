2c6ad52ca44a569bff3c72e97954b6a0d0f5262e
Edward Humpert <edwardmhumpert@gmail.com>
2023-03-24 19:34:39 -0400

    timer added, now to hard js stuff

--- index.js
+++ index.js
@@ -1,46 +1,72 @@
-// const keys = document.querySelectorAll('.key');
-// const textField = document.getElementById('text-field');
+// ok: so to get this going with a rando keyboard every time i've got
+// four rows :
+// 1st row 0-5 letters, 6 delete
+// 2nd row 0-6 letters
+// 3rd row 0-6 letters
+// 4th row 0-5 letters, 6 clear
+// timer
+// placeholder text in field
+// generate field when finished with one like in codeBreaker
+// once innertext matches word, new field appears / new keyboard appears, etc.
 
-// function playSound(e) {
-//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//   const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
-//   if (!audio) return;
-//   audio.currentTime = 0;
-//   audio.play();
-//   key.classList.add('playing');
-  
-//   textField.value += key.innerText; // Update text field value with pressed key
-// }
-
-// function removeTransition(e) {
-//   if (e.propertyName !== 'transform') return;
-//   this.classList.remove('playing');
-// }
-
-// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-// window.addEventListener('keydown', playSound);
-
-const keys = document.querySelectorAll('.key');
-const textField = document.getElementById('text-field');
-const currentValue = textField.value;
-const clearBtn = document.querySelector('.clear');
-const deleteBtn = document.querySelector('.delete');
+// let's get shuffle keyboard function down
 
 
-keys.forEach(key => {
-  key.addEventListener('click', () => {
-    textField.value += key.innerText;
+document.addEventListener("DOMContentLoaded", () => {
+  const keys = document.querySelectorAll(".key");
+  const textField = document.getElementById("text-field");
+  const currentValue = textField.value;
+  const clearBtn = document.querySelector(".clear");
+  const deleteBtn = document.querySelector(".delete");
+  let timer = document.getElementById("timer");
+  let startStopButton = document.getElementById("start-stop");
+  let intervalId;
+  let startTime;
+
+  keys.forEach((key) => {
+    key.addEventListener("click", () => {
+      textField.value += key.innerText;
+    });
   });
-});
 
-clearBtn.addEventListener('click', () => {
-    textField.value = '';
-});
+  clearBtn.addEventListener("click", () => {
+    textField.value = "";
+  });
 
-deleteBtn.addEventListener('click', deleteLastCharacter);
+  deleteBtn.addEventListener("click", deleteLastCharacter);
 
-function deleteLastCharacter() {
-    const textField = document.getElementById('text-field');
+  function deleteLastCharacter() {
+    const textField = document.getElementById("text-field");
     const currentValue = textField.value;
     textField.value = currentValue.substring(0, currentValue.length - 1);
   }
+
+  function startTimer() {
+    startTime = Date.now();
+    intervalId = setInterval(updateTimer, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(intervalId);
+  }
+
+  function updateTimer() {
+    let elapsedTime = Math.floor((Date.now() - startTime) / 1000); // calculate elapsed time in seconds
+    let hours = Math.floor(elapsedTime / 3600);
+    let minutes = Math.floor((elapsedTime - hours * 3600) / 60);
+    let seconds = elapsedTime - hours * 3600 - minutes * 60;
+    timer.innerText = `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  }
+
+  startStopButton.addEventListener("click", function () {
+    if (startStopButton.innerText === "Start") {
+      startStopButton.innerText = "Stop";
+      startTimer();
+    } else {
+      startStopButton.innerText = "Start";
+      stopTimer();
+    }
+  });
+});
